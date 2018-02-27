1fa389ad241b7461e9c02e9f16cc4c3b7acf4190
sandor13 <sandor13@lab104-22.iit.uni-miskolc.hu>
2018-02-27 16:24:47 +0100

    Gyak3

--- masodfoku/src/masodfoku/masodfoku.java
+++ masodfoku/src/masodfoku/masodfoku.java
@@ -1,27 +1,46 @@
 package masodfoku;
 
+import java.util.Scanner;
+
 public class masodfoku {
 	public static void main(String[] args){
 		
-		int a=5;
-		int b=2;
-		int c=2;
+		int a=0;
+		int b=0;
+		int c=0;
 		
 		double x1=0;
 		double x2=0;
 		
 		double diszkriminans=0;
 		
+		System.out.println("Kérem add meg a változókat: ");
+		Scanner in = new Scanner(System.in);
 		
-		diszkriminans=(Math.sqrt((b*b)-4*a*c));
+		
+		
+		System.out.println("A valtozo: ");
+		a=in.nextInt();
+		System.out.println("B valtozo: ");
+		b=in.nextInt();
+		System.out.println("C valtozo: ");
+		c=in.nextInt();
+		
+		System.out.println("Az a valtozo: "+a);
+		System.out.println("A b valtozo: "+b);
+		System.out.println("A c valtozo: "+c);
+		
+		
+		
+		
+		diszkriminans = (b*b)-4*a*c;
 		System.out.println("Diszkriminans erteke: "+diszkriminans);
 		
 		
-		if (diszkriminans<0) {
+		if ( diszkriminans < 0) {
 			System.out.println("Nincs megoldas");
-			
-		}else { 
-			System.out.println("Van megoldasa");
+		}
+		else { 
 		x1= b*(-1) + diszkriminans / 2*a;
 		x2=b*(-1) - diszkriminans/2 *a;
 		System.out.println("A megoldasok x1: "+x1+"  x2:  "+x2);
