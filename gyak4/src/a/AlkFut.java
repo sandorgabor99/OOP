83e93fa4c16548352d1655b905b13ecac6840c26
sandor13 <sandor13@lab104-22.iit.uni-miskolc.hu>
2018-03-06 17:45:46 +0100

    Gyak4

--- gyak4/src/a/AlkFut.java
+++ gyak4/src/a/AlkFut.java
@@ -5,7 +5,56 @@ public class AlkFut {
 	public static void main(String[] args) {
 		Alkalmazott alkalmazott1 = new Alkalmazott();
 		
-		alkalmazott1.setName("Tomi");		
+		Alkalmazott[] alkok = new Alkalmazott[5];
+		
+		alkok[0]=new Alkalmazott();
+		alkok[0].setName("Tomi1");
+		alkok[0].setpayment(10);
+		
+
+		alkok[1]=new Alkalmazott();
+		alkok[1].setName("Tomi2");
+		alkok[1].setpayment(10);
+		
+
+		alkok[2]=new Alkalmazott();
+		alkok[2].setName("Tomi3");
+		alkok[2].setpayment(10);
+		
+
+		alkok[3]=new Alkalmazott();
+		alkok[3].setName("Tomi4");
+		alkok[3].setpayment(150);
+		
+
+		alkok[4]=new Alkalmazott();
+		alkok[4].setName("Tomi5");
+		alkok[4].setpayment(78);
+		
+		
+		for (Alkalmazott a : alkok)
+			System.out.println(a.getSzoveg());
+		int max = alkok[0].getpayment();
+		for (int i = 0; i < alkok.length; i++) {
+			if (alkok[i].getpayment() > max)
+				max = alkok[i].getpayment();
+			
+		}
+		System.out.println(" A legnagyobb fizetesu : "+ max);
+		
+		for(int i=0;i<alkok.length;i++){
+			
+			if(alkok[i].getpayment() == max)
+				System.out.println("Adatai:\n "+ alkok[i].getSzoveg());
+		}
+		
+		
+		
+		
+		
+		
+		
+	/*	alkalmazott1.setName("Tomi");		
 		alkalmazott1.setpayment(10);
 		
 		
@@ -38,6 +87,6 @@ public class AlkFut {
 				System.out.println("Az alk2 fizetese nagyobb");
 				
 			}
-			
+	*/		
 	}
 }
